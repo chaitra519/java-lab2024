4487d21876771a5668fb2c3a94ffc7523a4233c3
chaitra519 <chagantipatichaitra@gmail.com>
2024-08-07 10:25:09 +0530

    Add files via upload

---
+++ Evenandodd.java
@@ -0,0 +1,17 @@
+class Evenandodd{
+public static void main(String args[])
+{
+int a[]={26,75,43,6,8,97};
+int i;
+for(i=0;i<a.length;i++)
+{
+if(a[i]%2==0)
+System.out.println(a[i]+"");
+}
+for(i=0;i<a.length;i++)
+{
+if(a[i]%2!=0)
+System.out.println(a[i]+"");
+}
+}
+}
